51596aa9e7bc9688f4b64138cf34910cd6a0687c
Ashu Pednekar <ashupednekar49@gmail.com>
2024-12-10 04:04:19 +0530

    feat: day3 implementation complete

--- 2024/day3/src/main.rs
+++ 2024/day3/src/main.rs
@@ -2,6 +2,7 @@ use std::{error::Error, fs::read_to_string};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let s = read_to_string("input.txt")?;
+    //let s = "select(){,(where()+-mul(514,727)".to_string();
     let mut iter = s.char_indices().peekable();
     let mut res = 0;
     while let Some((i, c)) = iter.next() {
@@ -11,13 +12,21 @@ fn main() -> Result<(), Box<dyn Error>> {
             iter.clone().nth(2),
         ) {
             if c == 'm' && c1 == 'u' && c2 == 'l' && c3 == '(' {
-                for j in 4..=8 {
+                for j in 4..=10 {
                     if let Some(a) = iter.clone().nth(j) {
+                        if a.1 == '(' {
+                            break;
+                        }
                         if a.1 == ')' {
-                            let tmp = String::from(&s[i..i+j+1]).replace("mul(", "");
-                            let l = tmp.split(",").map(|num| num.parse().unwrap()).collect::<Vec<i32>>();
-                            println!("multiplying {} X {}", &l[0], &l[1]);
-                            res += l[0] * l[1];
+                            let tmp = String::from(&s[i..i + j + 1]).replace("mul(", "");
+                            let l = tmp
+                                .split(",")
+                                .filter_map(|num| num.parse().ok())
+                                .collect::<Vec<i32>>();
+                            if l.len() == 2 {
+                                println!("multiplying {} X {}", &l[0], &l[1]);
+                                res += l[0] * l[1];
+                            }
                             break;
                         }
                     }
